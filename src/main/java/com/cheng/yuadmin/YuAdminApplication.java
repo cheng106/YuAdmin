71d1a78e50918873de85a1056c4456f969fcd55f
Cheng® <cheng106.andy@gmail.com>
2022-02-12 15:06:10 +0800

    feat: init project

---
+++ src/main/java/com/cheng/yuadmin/YuAdminApplication.java
@@ -0,0 +1,13 @@
+package com.cheng.yuadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuAdminApplication.class, args);
+    }
+
+}
